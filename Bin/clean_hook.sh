a95f63ce0fdf96a6b66e02d7aa4d72c8ea42426d
JackA1ltman <cs2dtzq@163.com>
2026-01-06 19:41:23 +0800

    feat: merged mainline.
    Change-Id: I70825baa71a684f6f9c680abc23a5fab1fd791eb
    Signed-off-by: JackA1ltman <cs2dtzq@163.com>

--- Bin/clean_hook.sh
+++ Bin/clean_hook.sh
@@ -3,11 +3,25 @@
 # Tested kernel versions: 5.4, 4.19, 4.14, 4.9, 4.4, 3.18
 # 20251120
 
+KSU_FOLDER=("drivers/kernelsu" "KernelSU" "KernelSU-Next")
+
 KSU_CLEAN_FILES=("fs/exec.c" "fs/read_write.c" "fs/open.c" "fs/stat.c" "fs/devpts/inode.c" "drivers/input/input.c" "drivers/tty/pty.c" "security/selinux/hooks.c" "kernel/reboot.c" "kernel/sys.c")
 
 SUSFS_CLEAN_FILES=("security/selinux/avc.c" "kernel/kallsyms.c" "kernel/sys.c" "kernel/reboot.c" "fs/statfs.c" "fs/namespace.c" "fs/proc_namespace.c" "fs/stat.c" "fs/namei.c" "fs/readdir.c" "fs/exec.c" "fs/proc/task_mmu.c" "fs/proc/base.c" "fs/proc/fd.c" "fs/proc/cmdline.c" "fs/overlayfs/super.c" "fs/overlayfs/overlayfs.h" "fs/overlayfs/inode.c" "fs/overlayfs/inode.c" "fs/notify/fdinfo.c" "fs/devpts/inode.c")
 
 SUSFS_REMAIN_CLEAN_FILES=("fs/susfs.c" "include/linux/susfs.h" "include/linux/susfs_def.h")
+
+# Removal of KernelSU
+
+for file in "${KSU_FOLDER[@]}"; do
+    rm -rf "${file}"
+
+    if [ -f "${file}" ] || [ -d "${file}" ]; then
+        echo "[-] Could not remove ${file}."
+    else
+        echo "[+] Cleaned for ${file}."
+    fi
+done
 
 # Removal of KernelSU Hook
 
